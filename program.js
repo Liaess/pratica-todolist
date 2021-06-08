39150b15e0ee652ec3bfbcd4125d085009ab8794
Yann <yann.melo@gmail.com>
2021-06-08 14:49:46 -0300

    Check feature with small bug

--- program.js
+++ program.js
@@ -1,40 +1,43 @@
 import readlineSync from "readline-sync";
 
-const questions = ["add", "list","check","remove"];
+const questions = ["Add", "List","Check","Remove"];
 const todolist = [];
 
 function ToDoList(){
     while(true){
         const index = readlineSync.keyInSelect(questions, "Type your command");
+        
         if(index === 0){
             const action = readlineSync.question("What do you want to do?");
             todolist.push(`🔴 ${action}`);
         }
+
         if(index === 1){
-            if(todolist.length < 1) console.log("Empty list")
+            if(todolist.length < 1) console.log("Empty list");
             else{
-                console.log("=============================")
+                console.log("=============================");
                 for (let i = 0; i < todolist.length; i++) {
                     console.log(todolist[i])
                 }
-                console.log("=============================")
+                console.log("=============================");
             }
         }
+
         if(index === 2){
-            const index = readlineSync.keyInSelect(todolist, "What do you want to check/uncheck?");
-            console.log(index)
-            if(index !== -1){
-                todolist[index].replace(`🔴 ${todolist[index]}`, `🟢 ${todolist[index]}`);
+            const selected = readlineSync.keyInSelect(todolist, "What do you want to check/uncheck?");
+            if(selected !== -1){
+                todolist[selected].includes("🔴") ? todolist[selected] = "🟢" + todolist[selected].substring(1) : "🔴" + todolist[selected].substring(1)
             }
         }
+
         if(index === 3){
             const remove = readlineSync.keyInSelect(todolist, "What do you want to remove?");
             todolist.splice(remove, 1);
         }
+        
         if(index === -1){
             return
         }
-
     }
 }
 
